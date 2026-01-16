ca8fa3e53bbaf2eec6026506ffef66a2604d4732
Rushikesh <rushikeshshinde6200@gmail.com>
2026-01-16 11:01:17 +0530

    Falling text success

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './Components/Navbar.jsx'
 import Hero from './Components/Hero.jsx'
 import About from './Components/About.jsx'
 import Footer from './Components/Footer.jsx'
-
+import FallingText from './complex/FallingText.jsx'
 function App() {
 
   return (
@@ -12,8 +12,21 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <div className='h-[200px] border-t-2 border-b-2 border-gray-300 my-20 flex items-center justify-center'>
 
-      <Footer />
+    <FallingText
+      text={`React Bits is a library of animated and interactive React components designed to streamline UI development and simplify your workflow.`}
+      highlightWords={["React", "Bits", "animated", "components", "simplify"]}
+      highlightClass="highlighted"
+      trigger="hover"
+      backgroundColor="transparent"
+      wireframes={false}
+      gravity={0.56}
+      fontSize="2rem"
+      mouseConstraintStiffness={0.9}
+    />      
+      </div>
+    <Footer />
     </>
   )
 }
